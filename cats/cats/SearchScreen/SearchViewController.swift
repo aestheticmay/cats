bdfc81636c4075b6e08795a4da029fe27992a6c0
aestheticmay <60157413+aestheticmay@users.noreply.github.com>
2022-11-13 16:57:18 +0300

    Not ready

--- cats/cats/SearchScreen/SearchViewController.swift
+++ cats/cats/SearchScreen/SearchViewController.swift
@@ -11,6 +11,15 @@ final class SearchViewController: UIViewController {
     
     // MARK: - Private Properties
     
+    enum TableSection: Int {
+        case userList
+        case loader
+    }
+    
+    private var pageLimit = 0
+    private var currentLastId: Int? = nil
+    private var isNewDataLoading = false
+    
     private let tableView: UITableView = {
         let tbv = UITableView()
         tbv.backgroundColor = .clear
@@ -25,19 +34,28 @@ final class SearchViewController: UIViewController {
     
     private var catsModel = [CatModel]()
     
+    private lazy var activityIndicator = LoadMoreActivityIndicator(scrollView: self.tableView, spacingFromLastCell: 10, spacingFromLastCellWhenLoadMoreActionStart: 60)
+
     // MARK: - Life-Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-        fetchData(refresh: true)
+        reloadData()
         setupLayout()
     }
     
     // MARK: - Private Methods
     
     @objc func reloadData() {
-      fetchData(refresh: true)
+        fetchData() { [weak self] result in
+            switch result {
+            case .success(let model):
+                self?.catsModel = model
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     private func configureTableView() {
@@ -47,36 +65,29 @@ final class SearchViewController: UIViewController {
         tableView.refreshControl?.addTarget(self, action: #selector(reloadData), for: .valueChanged)
     }
     
-    func fetchData(refresh: Bool = false) {
-        
-        if refresh {
-            self.tableView.refreshControl?.beginRefreshing()
-        }
-
+    func fetchData(limit: Int = 10, completion: @escaping (Result<[CatModel], Error>) -> Void) {
+       
         guard var endpoint = URLComponents(string: (ApiClient.ApiClientEndpoint.allCats.urlString())) else { return }
         print(endpoint)
         
         var queryParameters: [String: String] = [:]
-        queryParameters["limit"] = "5"
+        queryParameters["limit"] = String(limit)
         queryParameters["size"] = "small"
-
+      //  queryParameters["page"] = "1" TODO: Add pagination
+        
         endpoint.queryItems = queryParameters.map({ (key, value) in
             URLQueryItem(name: key, value: value)
         })
-
+        
         guard let url = endpoint.url else {
             return
         }
-       
+        
         var request = URLRequest(url: url)
         request.setValue(ApiClient.Identifiers.apiKey, forHTTPHeaderField: "x-api-key")
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             DispatchQueue.main.async {
-                if refresh {
-                    self.tableView.refreshControl?.endRefreshing()
-                }
-                
                 if error != nil {
                     print(error.debugDescription)
                     
@@ -93,7 +104,24 @@ final class SearchViewController: UIViewController {
         }
         task.resume()
     }
- 
+  
+    
+    private func fetch(completed: ((Bool) -> Void)? = nil) {
+        fetchData() { [weak self] result in
+            switch result {
+            case .success(let model):
+                // 5
+                self?.catsModel.append(contentsOf: model)
+                // assign last id for next fetch
+                completed?(true)
+            case .failure(let error):
+                print(error.localizedDescription)
+                // 6
+                completed?(false)
+            }
+        }
+    }
+    
     private func setupLayout() {
         view.backgroundColor = UIColor.white
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Academy Engraved LET", size: 25)!]
@@ -105,6 +133,13 @@ final class SearchViewController: UIViewController {
             make.top.equalToSuperview().inset(5)
             make.leading.trailing.equalToSuperview().inset(10)
             make.bottom.equalToSuperview()
+        }
+    }
+    
+    private func hideBottomLoader() {
+        DispatchQueue.main.async {
+            let lastListIndexPath = IndexPath(row: self.catsModel.count - 1, section: TableSection.userList.rawValue)
+            self.tableView.scrollToRow(at: lastListIndexPath, at: .bottom, animated: true)
         }
     }
 }
@@ -126,12 +161,24 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         let detailsViewController = DetailsViewController(cats: catsModel[indexPath.row])
         navigationController?.pushViewController(detailsViewController, animated: true)
     }
-   /*
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == catsModel.count - 1 {
-            reloadData()
-            
-        }
     
-        */
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        activityIndicator.start {
+            DispatchQueue.global(qos: .utility).async {
+                sleep(1)
+                self.pageLimit += 10
+                self.fetchData(limit: self.pageLimit) { [weak self] result in
+                    switch result {
+                    case .success(let model):
+                        self?.catsModel = model
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+                DispatchQueue.main.async { [weak self] in
+                    self?.activityIndicator.stop()
+                }
+            }
+        }
+    }
 }
