b5c75bd79a60204fd95856244ff18b85eff6d02f
aestheticmay <60157413+aestheticmay@users.noreply.github.com>
2022-11-22 20:59:04 +0300

    Tabbar

---
+++ cats/cats/FavoritesScreen/FavoritesViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FavoritesViewController.swift
+//  cats
+//
+//  Created by Майя Калицева on 22.11.2022.
+//
+
+import UIKit
+
+final class FavoritesViewController: UIViewController {
+    
+}
